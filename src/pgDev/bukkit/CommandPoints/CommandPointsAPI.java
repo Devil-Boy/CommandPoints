33a09e6203881f1fb2b4e58c9cd6847ad13bce2e
Devil boy <kws512@hotmail.com>
2011-10-25 18:50:11 -0700

    Forgot to throw in this file...

---
+++ src/pgDev/bukkit/CommandPoints/CommandPointsAPI.java
@@ -0,0 +1,56 @@
+package pgDev.bukkit.CommandPoints;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * CommandPoints for Bukkit
+ *
+ * @author PG Dev Team
+ * @author Devil Boy
+ */
+public class CommandPointsAPI {
+	CommandPoints cp;
+	
+	/**
+	 * CommandPointsAPI Constructor
+	 * @param playerName The player receiving the points
+	 * @param amount How many points
+	 * @param reason Why he's getting the points
+	 */
+	public CommandPointsAPI(CommandPoints pluginI) {
+		cp = pluginI;
+	}
+	
+	
+	// Developer API Functions and Methods (API)
+    
+    /**
+	 * Give points to a player
+	 * @param playerName The player receiving the points
+	 * @param amount How many points
+	 * @param reason Why he's getting the points
+	 */
+	public void addPoints(String playerName, double amount, String reason, Plugin plugin) {
+		cp.addPoints(playerName, amount, reason, plugin);
+	}
+	
+	/**
+	 * Remove points from a player
+	 * @param playerName The player losing the points
+	 * @param amount How many points
+	 * @param reason Why he's losing the points
+	 */
+	public void removePoints(String playerName, double amount, String reason, Plugin plugin) {
+		cp.removePoints(playerName, amount, reason, plugin);
+	}
+	
+	/**
+	 * Get how many points a player has
+	 * @param playerName The player we want to find about
+	 * @return How many points the player has
+	 */
+	public double getPoints(String playerName, Plugin plugin) {
+		return cp.checkPoints(playerName, plugin);
+	}
+
+}
